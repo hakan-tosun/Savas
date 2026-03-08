5f31dbc8fbdc2b4014e5fe0772eebf640d0e780f
Hakan Tosun <56801897+hakantsn@users.noreply.github.com>
2026-03-08 16:34:22 +0300

    Uçaklar hareket ettirildi ve mermilerle vurulup yok olmaları sağlandı.

--- SavasLibrary/Concrete/Ucak.cs
+++ SavasLibrary/Concrete/Ucak.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,9 +14,22 @@ namespace SavasLibrary.Concrete
         private static readonly Random Random = new Random();
         public Ucak(Size hareketAlaniBoyutlari) : base(hareketAlaniBoyutlari)
         {
+            HareketMesafesi = (int)(Height * .1);
+
             int sutunSayisi = hareketAlaniBoyutlari.Width / Width;
             int rastgeleSutun = Random.Next(0, sutunSayisi);
             Left = rastgeleSutun * Width;
+        }
+
+        public Mermi VurulduMu(List<Mermi> mermiler)
+        {
+            foreach (var mermi in mermiler)
+            {
+                var vurulduMu = mermi.Top < Bottom && mermi.Right > Left && mermi.Left < Right;
+                if (vurulduMu) return mermi;
+            }
+
+            return null;
         }
     }
 }
